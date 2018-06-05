629bc4a7e8b46e0bee9a265bbb4c363215f5ef9b
haniffrozen <37571635+haniffrozen@users.noreply.github.com>
2018-06-05 14:15:29 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=N0t6Um9oNRvBTuTzg4AFhc5QJ8yi5vQflhxzV44h";
         
         $("#loadImage").click(function(){
             $.ajax({
